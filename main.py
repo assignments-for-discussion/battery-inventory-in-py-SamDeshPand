b223894da4a51f9122e194dade3aa61fe76fc090
SamDeshPand <113538770+SamDeshPand@users.noreply.github.com>
2023-12-03 17:20:40 +0530

    Updated count_batteries_by_health function

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 
 def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
+  dict={ "healthy": 0, "exchange": 0, "failed": 0}
+  for battery in present_capacities:
+    SoH=float(100*(battery)/120)
+    if SoH>80:
+      dict["healthy"]=dict["healthy"]+1
+    elif SoH<80 and SoH>62:
+      dict["exchange"]=dict["exchange"]+1
+    else :
+      dict["failed"]=dict["failed"]+1
+  return dict
 
 
 def test_bucketing_by_health():
